32296ea2f4e8fc5db81ba5fb58f6c268319c6bea
JoelHernandez343 <joel.programador@gmail.com>
2021-03-04 14:07:50 -0600

    Added lesson 1

---
+++ lessons/01/hello.c
@@ -0,0 +1,11 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+int init_module(void){
+    printk(KERN_INFO"Hello world!, initializing the module 1.\n");
+    return 0;
+}
+
+void cleanup_module(void){
+    printk(KERN_INFO"Finising the execution of the module.\n");
+}
