f62fc601b8df73c81033847d4ab25d7250480286
JoelHernandez343 <joel.programador@gmail.com>
2021-04-05 23:48:57 -0500

    Added lesson kernel modules with threads 4

---
+++ lessons/thread04/thread-4.c
@@ -0,0 +1,98 @@
+/*
+ *  thread-4.c - Demostracion de la ejecución de un hilo en espacio de kernel
+ *  se usa la función kthread_create() y wake_up_process(). Se vinvula el hilo
+ *  a un procesador con la funcion kthread_bind()
+ */
+#include <linux/module.h>	/* Necesaria para todos los modulos 		*/
+#include <linux/kernel.h>	/* Necesaria para KERN_INFO 			*/
+#include <linux/init.h>		/* Necesaria para las macros de documentacion	*/
+#include <linux/moduleparam.h>	/* Necesaria para las macros de parametros 	*/
+#include <linux/kthread.h>	/* Necesaria para los hilos 			*/
+#include <linux/delay.h>	/* Necesaria para las funciones de tiempo	*/
+#define MAX 5
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("VICTOR HUGO GARCIA ORTEGA");
+MODULE_DESCRIPTION("ESTE ES UN EJEMPLO DE MODULO DE KERNEL");
+MODULE_VERSION("4.0");
+MODULE_INFO(driver, "PULSO CARDIACO");
+MODULE_INFO(interfaz, "UART");
+
+/*
+static int irq = 10;
+module_param( irq, int, 0660 );
+MODULE_PARM_DESC( irq, "Número de interrupción" );
+
+static char *comando = "derecha";
+module_param( comando, charp, 0660 );
+MODULE_PARM_DESC( comando, "Comando del motor" );
+
+static int datos[MAX] = {1, 2, 3, 4, 5};
+static int numEle = MAX;
+module_param_array( datos, int, &numEle, 0660 );
+MODULE_PARM_DESC( datos, "Muestras" );
+*/
+struct task_struct *khilo;
+
+static int hilo_kernel( void *arg )
+{
+	int id = *(int *)arg;
+	int cont = 0;
+	int cpu;
+
+	printk(KERN_INFO "Iniciando la ejecucion de la funcion hilo_kernel\n");
+
+	while( !kthread_should_stop() )
+	{
+		schedule();
+		//ssleep(1);
+		cpu = smp_processor_id();
+		printk(KERN_INFO "Ejecucion en CPU: %d contador: %d \n", cpu, cont++);
+	}
+
+	return id;
+}
+
+static int __init funcion_inicio(void)
+{
+//	register int i;
+	static int id_thread = 10;
+
+	printk(KERN_INFO "Iniciando el ejemplo 1 de hilos de kernel\n");
+//	printk(KERN_INFO "La irq en la funcion inicio es: %d \n", irq);
+	khilo = kthread_create( hilo_kernel, (void *)&id_thread, "kmi_hilo" );
+
+	if( IS_ERR(khilo) )
+	{
+		printk(KERN_ERR "Error al crear el hilo de kernel\n");
+
+		return PTR_ERR(khilo);
+	}
+	kthread_bind( khilo, 3 );
+	wake_up_process( khilo );
+	printk(KERN_INFO "Hilo creado con pid: %d y nombre: %s\n", khilo->pid, khilo->comm);
+
+	/*
+	 * Un valor de retorno diferente de 0 significa que
+	 * init_module fallo; el modulo no se puede cargar
+	 */
+	return 0;
+}
+
+static void __exit funcion_exit(void)
+{
+	int ret_hilok = kthread_stop( khilo );
+
+	printk(KERN_INFO "Terminando la ejecucion del ejemplo 1 de hilos\n");
+
+	if( ret_hilok == -EINTR )
+		printk(KERN_ERR "Error en la terminacion del hilo\n");
+	else
+		printk(KERN_INFO "Terminacion del hilo con valor: %d \n", ret_hilok);
+
+//	printk(KERN_INFO "La irq en la funcion exit es: %d \n", irq);
+
+}
+
+module_init( funcion_inicio );
+module_exit( funcion_exit );
