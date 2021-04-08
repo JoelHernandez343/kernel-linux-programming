59cb53286123d0158204d551cd014998b7c30440
JoelHernandez343 <joel.programador@gmail.com>
2021-04-08 16:00:49 -0500

    Added app structure

---
+++ practices/02-strings/client/src/app.js
@@ -0,0 +1,52 @@
+// const { showAbout } = require('./modules/about');
+// const { menu, pause, askArray, askOption } = require('./modules/inquierer');
+// const {
+//   setParameter,
+//   showParameter,
+//   showAllParameters,
+// } = require('./modules/parameters');
+
+const app = async () => {
+  let option = 0;
+
+  do {
+    // option = await menu();
+    // await switchOpt(option);
+    // await pause();
+  } while (option !== 0);
+};
+
+// const switchOpt = async option => {
+//   switch (option) {
+//     case 1:
+//       showAllParameters();
+//       break;
+
+//     case 'option':
+//     case 'data':
+//     case 'evenData':
+//     case 'average':
+//       showParameter(option);
+//       break;
+
+//     case 6:
+//       setParameter('data', await askArray());
+//       break;
+
+//     case 7:
+//       const opt = await askOption();
+//       if (opt !== 0) {
+//         setParameter('option', opt.toString());
+//       }
+//       break;
+
+//     case 8:
+//       showAbout();
+//       break;
+
+//     default:
+//       break;
+//   }
+// };
+
+module.exports = app;
