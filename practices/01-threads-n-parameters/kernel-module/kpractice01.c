3a07d7c1d4c682cf17a9128b8f1b1aee3134e806
JoelHernandez343 <joel.programador@gmail.com>
2021-04-07 21:02:20 -0500

    Finished kernel module part of practice 01

---
+++ practices/01-threads-n-parameters/kernel-module/kpractice01.c
@@ -0,0 +1,150 @@
+/*
+ * Joel Harim Hernández Javier @ 2021
+ * This code is under MIT license.
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/moduleparam.h>
+#include <linux/kthread.h>
+#include <linux/err.h>
+
+#define MAX 254
+
+/* Module information */
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("JOEL HARIM HERNÁNDEZ JAVIER");
+MODULE_DESCRIPTION("Practice with kernel module n threads.");
+MODULE_VERSION("1.0.0");
+
+/* Parameters */
+static int option = 0;
+module_param(option, int, 0660);
+MODULE_PARM_DESC(option, "[1: Average, 2: Sorting, 3: Even numbers]");
+
+static int data[MAX];
+static int length = MAX;
+module_param_array(data, int, &length, 0660);
+MODULE_PARM_DESC(data, "Array to sort.");
+
+static int even_data[MAX];
+module_param_array(even_data, int, &length, 0440);
+MODULE_PARM_DESC(even_data, "Result array for even numbers.");
+
+static int average = 0;
+module_param(average, int, 0440);
+MODULE_PARM_DESC(average, "Average result.");
+
+
+/* Thread functions */
+
+// Simple bubble sort. O(n^2)
+void sort(void){
+    register int i, j;
+    for (i = 0; i < length; ++i){
+        for (j = i + 1; j < length; ++j){
+            if (data[i] > data[j]){
+                int aux = data[i];
+                data[i] = data[j];
+                data[j] = aux;
+            }
+        }
+    }
+}
+
+// Store the data's even numbers in even_data
+void find_even_numbers(void){
+    register int i, j;
+    i = j = 0;
+
+    for (j = 0; j < length; ++j){
+        if ((data[j] & 1) == 0){
+            even_data[i++] = data[j];
+        }
+    }
+
+    for (; i < length; ++i){
+        even_data[i] = -1;
+    }
+}
+
+// Calculate the data's average and store in average
+void calc_average(void){
+    register int i;
+    int result = 0;
+
+    for (i = 0; i < length; ++i){
+        result += data[i];
+    }
+
+    average = result / length;
+}
+
+struct task_struct *kthread;
+
+static int thread_fn(void *args){
+    int id = *(int *)args;
+
+    printk(KERN_INFO"Thread worker has been started.\n");
+
+    while (!kthread_should_stop()){
+        schedule();
+
+        switch (option) {
+            case 1:
+                calc_average();
+                break;
+        
+            case 2:
+                sort();
+                break;
+
+            case 3:
+                find_even_numbers();
+                break;
+        }
+
+        option = 0;
+    }
+
+    return id;
+}
+
+/* Init and exit funcions */
+static int __init init_fn(void){
+    static int thread_id = 1;
+
+    printk(KERN_INFO"Initializing practice 1 :)\n");
+
+    kthread = kthread_create(thread_fn, (void *)&thread_id, "kpractice1_thread");
+
+    if (IS_ERR(kthread)){
+        printk(KERN_ERR"Error creating thread, leaving.\n");
+
+        return PTR_ERR(kthread);
+    }
+
+    wake_up_process(kthread);
+    printk(KERN_INFO"Thread created with pid: %d and name: %s.\n", kthread->pid, kthread->comm);
+
+    return 0;
+}
+
+static void __exit exit_fn(void){
+    int thread_ret;
+
+    printk(KERN_INFO"Finishing execution...\n");
+
+    thread_ret = kthread_stop(kthread);
+
+    if (thread_ret == -EINTR){
+        printk(KERN_ERR"Error finishing thread :(\n");
+        return;
+    }
+
+    printk(KERN_ERR"Thread finished with value %d\n", thread_ret);
+}
+
+module_init(init_fn);
+module_exit(exit_fn);
