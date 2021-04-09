43dfad2570cf38707699b6f541efc1b48f549773
JoelHernandez343 <joel.programador@gmail.com>
2021-04-08 20:33:51 -0500

    Changed camel case fn to underscore name

--- practices/01-kernel-thread/kernel-module/kpractice01.c
+++ practices/01-kernel-thread/kernel-module/kpractice01.c
@@ -81,7 +81,7 @@ void calc_average(void){
     average = result / length;
 }
 
-void showArray(const int *array, const char *name){
+void show_array(const int *array, const char *name){
     register int i;
     for (i = 0; i < length; ++i){
         printk(KERN_INFO"%s[%d]: %d.\n", name, i, array[i]);
@@ -108,13 +108,13 @@ static int thread_fn(void *args){
             case 2:
                 printk(KERN_INFO"Method invoked. Sorting data.\n");
                 sort();
-                showArray(data, "data");
+                show_array(data, "data");
                 break;
 
             case 3:
                 printk(KERN_INFO"Method invoked. Finding even numbers.\n");
                 find_even_numbers();
-                showArray(even_data, "even_data");
+                show_array(even_data, "even_data");
                 break;
         }
 
